d4041998df377926fc01e0d465bf87c0ae2871ae
taksh mehta <takshmehta111@gmail.com>
2020-07-28 23:31:27 +0530

    Updates

---
+++ precache-manifest.e7bd8e3c8d12fde2f7cd489bbcf58b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27160556885624a05a47e599630c0ec",
+    "url": "/advance-todo/index.html"
+  },
+  {
+    "revision": "d0b5ea4a000bdfd6e03a",
+    "url": "/advance-todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c01b00c003c967aa9f8f",
+    "url": "/advance-todo/static/css/main.72fc6cd0.chunk.css"
+  },
+  {
+    "revision": "d0b5ea4a000bdfd6e03a",
+    "url": "/advance-todo/static/js/2.ab8d0e54.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/advance-todo/static/js/2.ab8d0e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01b00c003c967aa9f8f",
+    "url": "/advance-todo/static/js/main.b337693d.chunk.js"
+  },
+  {
+    "revision": "a6f3010a480d1d82d10e",
+    "url": "/advance-todo/static/js/runtime-main.f413b880.js"
+  }
+]);
